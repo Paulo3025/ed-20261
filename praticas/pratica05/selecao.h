ba97af3822ad92bfae1e4fe80c028c3e06a364ee
PAULO <paulo.firmino@iesb.edu.br>
2026-04-22 14:36:40 -0300

    feat: conclui pratica05. Fecha #ID

---
+++ praticas/pratica05/selecao.h
@@ -0,0 +1,10 @@
+
+#ifndef SELECAO_H
+#define SELECAO_H
+
+
+int linear_search(int vetor[], int tamanho, int elemento_alvo);
+
+int quick_select(int vetor[], int inicio, int fim, int k);
+
+#endif // SELECAO_H
